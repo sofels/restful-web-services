98b7fb286361c4904bc84cf0598f0ec26a9d9c0f
Samuel Sofela <114030952+sofels@users.noreply.github.com>
2023-06-05 14:19:17 -0600

    Step 19 completed - Internationalizatoin

--- src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HallowWorldController.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/helloworld/HallowWorldController.java
@@ -1,13 +1,24 @@
 package com.example.rest.webservices.restfulwebservices.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 //REST API
 @RestController
 public class HallowWorldController {
+
+    private MessageSource messageSource;
+
+    public HallowWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "hello-world")
     public  String helloWorld(){
@@ -26,5 +37,13 @@ public class HallowWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         //return new HelloWorldBean("Hello World Path" + name);
         return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
+
+    @GetMapping(path = "hello-world-internationalized")
+    public String helloWorldPathVariable(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",
+                null, "Default Message", locale);
+        //return new HelloWorldInternationalized("Hello World Path" + name);
     }
 }
