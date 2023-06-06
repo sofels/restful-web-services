f05b44752dd7aaa2926afcf40ed0f4a291172f94
Samuel Sofela <114030952+sofels@users.noreply.github.com>
2023-06-05 20:32:21 -0600

    Step 22 completed - Hateos and versioning

--- src/main/java/com/example/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/example/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,12 +1,17 @@
 package com.example.rest.webservices.restfulwebservices.user;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 @RestController
@@ -21,15 +26,19 @@ public class UserResource {
     @GetMapping(path = "users")
     public List<User> retrieveAllUsers(){
         return service.findAll();
-
     }
 
     @GetMapping(path = "users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
         if(user ==null)
             throw new UserNotFoundException("id:"+id);
-        return user;
+
+        EntityModel<User> entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
 
     }
 
