9d12e8ccea5f206072aa1b0d2f496d2ec844dfcb
Samuel Sofela <114030952+sofels@users.noreply.github.com>
2023-06-05 23:52:08 -0600

    Step 30 completed - Posts created

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,15 @@ values(10003,current_date(),'Jade');
 
 insert into user_details(id, birth_date,name)
 values(10004,current_date(),'Jomi');
+
+insert into post(id, description, user_id)
+values(20001, 'I want to learn spring boot', 10001);
+
+insert into post(id, description, user_id)
+values(20002, 'I want to learn devops', 10001);
+
+insert into post(id, description, user_id)
+values(20003, 'I get Azure', 10002);
+
+insert into post(id, description, user_id)
+values(20004, 'I want to learn AWS', 10002);
